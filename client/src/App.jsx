c313259a35a985e74562c8150233f04fea4985eb
Nitish Singh <nitishkumarsingh909@gmail.com>
2025-02-26 20:37:34 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="container max-w-4xl mx-auto">
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/components/CreateSnippet.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CreateSnippet = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default CreateSnippet;
